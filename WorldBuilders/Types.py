cb9aadd4b21345ae7b2284637867ab6cff4dd00b
AntoineRichard <antoine.richard@gatech.edu>
2023-01-28 12:31:28 +0100

    Added all the types needed to support all the primitives inside Isaac.

--- WorldBuilders/Types.py
+++ WorldBuilders/Types.py
@@ -54,8 +54,8 @@ class Transformation_T:
 
 @dataclasses.dataclass
 class Transformation2D_T(Transformation_T):
-    orientation: Rot2D_T
-    translation: Translation2D_T
+    orientation: Rot2D_T = Rot2D_T()
+    translation: Translation2D_T = Translation2D_T()
 
     def __post_init__(self):
         assert isinstance(self.orientation, Rot2D_T), "A 2D transform requires a 2D rotation."
@@ -63,8 +63,8 @@ class Transformation2D_T(Transformation_T):
 
 @dataclasses.dataclass
 class Transformation3D_T(Transformation_T):
-    orientation: Orientation_T
-    translation: Translation3D_T
+    orientation: Orientation_T = Quaternion_T()
+    translation: Translation3D_T = Translation3D_T()
 
     def __post_init__(self):
         assert isinstance(self.orientation, Rot3D_T), "A 3D transform requires a 3D orientation."
@@ -79,7 +79,7 @@ class Transformation3D_T(Transformation_T):
 @dataclasses.dataclass
 class Layer_T:
     output_space: int = 0
-    transform: Transformation_T = None
+    transform: Transformation_T = Transformation3D_T()
 
     def __post_init__(self):
         assert self.output_space > 0, "output_space must be larger than 0."
@@ -87,8 +87,8 @@ class Layer_T:
 
 @dataclasses.dataclass
 class Line_T(Layer_T):
-    xmax: float = None
-    xmin: float = None
+    xmax: float = -0.5
+    xmin: float = 0.5
     output_space: int = 1
 
     def __post_init__(self):
@@ -97,10 +97,10 @@ class Line_T(Layer_T):
 
 @dataclasses.dataclass
 class Circle_T(Layer_T):
-    radius: float = None
-    center: tuple = None
-    theta_min: tuple = 0
-    theta_max: tuple = np.pi*2
+    center: tuple = (0,0)
+    radius: float = 0.5
+    theta_min: float = 0
+    theta_max: float = np.pi*2
     alpha: float = 1
     beta: float = 1
     output_space: int = 2
@@ -117,10 +117,10 @@ class Circle_T(Layer_T):
 
 @dataclasses.dataclass
 class Plane_T(Layer_T):
-    xmax: float = None
-    xmin: float = None
-    ymax: float = None
-    ymin: float = None
+    xmin: float = -0.5
+    xmax: float = 0.5
+    ymin: float = -0.5
+    ymax: float = 0.5
     output_space: int = 2
 
     def __post_init__(self):
@@ -131,11 +131,11 @@ class Plane_T(Layer_T):
 
 @dataclasses.dataclass
 class Disk_T(Layer_T):
-    center: tuple = None
-    radius_min: float = None
-    radius_max: float = None
-    theta_min: tuple = 0
-    theta_max: tuple = np.pi*2
+    center: tuple = (0,0)
+    radius_min: float = 0
+    radius_max: float = 0.5
+    theta_min: float = 0
+    theta_max: float = np.pi*2
     alpha: float = 1
     beta: float = 1
     output_space: int = 2
@@ -153,13 +153,26 @@ class Disk_T(Layer_T):
         assert self.radius_max >= self.radius_min, "The maximum radius must be larger than the minimum radius."
 
 @dataclasses.dataclass
+class Polygon_T(Layer_T):
+    x_coords: tuple = ()
+    y_coords: tuple = ()
+    output_space: int = 2
+    use_rejection_sampling = True
+    use_image_sampling = False
+
+    def __post_init__(self):
+        super().__post_init__()
+        assert len(self.x_coords) == len(self.y_coords), "The size of the x and y coordinates must be the same."
+        assert (self.use_rejection_sampling or self.use_image_sampling), "Must used either rejection sampling or image sampling."
+
+@dataclasses.dataclass
 class Cube_T(Layer_T):
-    xmax: float = None
-    xmin: float = None
-    ymax: float = None
-    ymin: float = None
-    zmax: float = None
-    zmin: float = None
+    xmin: float = -0.5
+    xmax: float = 0.5
+    ymin: float = -0.5
+    ymax: float = 0.5
+    zmin: float = -0.5
+    zmax: float = 0.5
     output_space: int = 3
     transform: Transformation3D_T = None
 
@@ -174,13 +187,13 @@ class Cube_T(Layer_T):
 
 @dataclasses.dataclass
 class Sphere_T(Layer_T):
-    center: tuple = None
+    center: tuple = (0,0,0)
     radius_min: float = 0
-    radius_max: float = None
-    theta_min: tuple = 0
-    theta_max: tuple = np.pi*2
-    phi_min: tuple = 0
-    phi_max: tuple = np.pi*2
+    radius_max: float = 0.5
+    theta_min: float = 0
+    theta_max: float = np.pi*2
+    phi_min: float = 0
+    phi_max: float = np.pi*2
     alpha: float = 1
     beta: float = 1
     ceta: float = 1
@@ -207,17 +220,18 @@ class Sphere_T(Layer_T):
 
 @dataclasses.dataclass
 class Cylinder_T(Layer_T):
-    center: tuple = None
+    center: tuple = (0,0,0)
     radius_min: float = 0
     radius_max: float = None
-    theta_min: tuple = 0
-    theta_max: tuple = np.pi*2
-    height_min: tuple = 0
-    height_max: tuple = np.pi*2
+    theta_min: float = 0
+    theta_max: float = np.pi*2
+    height_min: float = -0.5
+    height_max: float = 0.5
     alpha: float = 1
     beta: float = 1
     output_space: int = 3
     transform: Transformation3D_T = None
+    use_cartesian_space = False
 
     def __post_init__(self):
         super().__post_init__()
@@ -233,6 +247,96 @@ class Cylinder_T(Layer_T):
         assert self.radius_max > self.radius_min, "The maximum radius must be larger than the minimum radius."
         assert self.height_max > self.height_min, "The maximum height must be larger than the minimum height."
 
+@dataclasses.dataclass
+class Cone_T(Layer_T):
+    center: tuple = (0,0,0)
+    radius_min: float = 0
+    radius_max: float = 0.5
+    theta_min: float = 0
+    theta_max: float = np.pi*2
+    height_min: float = -0.5
+    height_max: float = 0.5
+    alpha: float = 1
+    beta: float = 1
+    output_space: int = 3
+    transform: Transformation3D_T = None
+    use_cartesian_space = False
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.transform is not None:
+            assert isinstance(self.transform, Transformation3D_T), "A 3D object, like a Cylinder, requires a 3D transform."
+        assert self.output_space >= 3, "output_space must be greater or equal to 3."
+        assert self.alpha > 0, "The alpha value must be larger than 0."
+        assert self.beta > 0, "The beta value must be larger than 0."
+        assert self.theta_min >= 0, "The minimum value of theta must be larger than 0."
+        assert self.theta_max <= np.pi*2, "The maximum value of theta must be smaller than 2pi."
+        assert self.theta_max >= self.theta_min, "The maximum value of theta must be larger than self.theta_max."
+        assert self.radius_min > 0, "The minimal radius must be larger than 0."
+        assert self.radius_max > self.radius_min, "The maximum radius must be larger than the minimum radius."
+        assert self.height_max > self.height_min, "The maximum height must be larger than the minimum height."
+
+@dataclasses.dataclass
+class Torus_T(Layer_T):
+    center: tuple = (0,0,0)
+    radius1_min: float = 0
+    radius1_max: float = 0.5
+    radius2_min: float = 0
+    radius2_max: float = 0.25
+    theta1_min: float = 0
+    theta1_max: float = np.pi*2
+    theta2_min: float = 0
+    theta2_max: float = np.pi*2
+    alpha1: float = 1
+    beta1: float = 1
+    alpha2: float = 1
+    beta2: float = 1
+    output_space: int = 3
+    transform: Transformation3D_T = None
+    use_cartesian_space = False
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.transform is not None:
+            assert isinstance(self.transform, Transformation3D_T), "A 3D object, like a Cylinder, requires a 3D transform."
+        assert self.output_space >= 3, "output_space must be greater or equal to 3."
+        assert self.alpha1 > 0, "The alpha value must be larger than 0."
+        assert self.beta1 > 0, "The beta value must be larger than 0."
+        assert self.alpha1 > 0, "The alpha value must be larger than 0."
+        assert self.beta1 > 0, "The beta value must be larger than 0."
+        assert self.theta1_min >= 0, "The minimum value of theta must be larger than 0."
+        assert self.theta1_max <= np.pi*2, "The maximum value of theta must be smaller than 2pi."
+        assert self.theta2_min >= 0, "The minimum value of theta must be larger than 0."
+        assert self.theta2_max <= np.pi*2, "The maximum value of theta must be smaller than 2pi."
+        assert self.theta1_max >= self.theta1_min, "The maximum value of theta must be larger than self.theta_max."
+        assert self.theta2_max >= self.theta2_min, "The maximum value of theta must be larger than self.theta_max."
+        assert self.radius1_min > 0, "The minimal radius must be larger than 0."
+        assert self.radius2_min > 0, "The minimal radius must be larger than 0."
+        assert self.radius1_max > self.radius1_min, "The maximum radius must be larger than the minimum radius."
+        assert self.radius2_max > self.radius2_min, "The maximum radius must be larger than the minimum radius."
+
+@dataclasses.dataclass
+class Image_T(Layer_T):
+    resolution: tuple = (1,1)
+    path: str = None
+    data: np.ndarray([]) = None
+    func: function = lambda x: x
+    output_space: int = 2
+
+@dataclasses.dataclass
+class Mask_T(Layer_T):
+    resolution: tuple = (1,1)
+    path: str = None
+    data: np.ndarray([]) = None
+    output_space: int = 2
+
+@dataclasses.dataclass
+class NormalMap_T(Layer_T):
+    resolution: tuple = (1,1)
+    path: str = None
+    data: np.ndarray([]) = None
+    output_space: int = 2
+    output_as_quaternions: bool = True
 
 ##################################
 #                                #
@@ -345,3 +449,11 @@ class LinearInterpolationSampler_T(Sampler_T):
         super().__post_init__()
         assert type(self.min) is tuple, "min must be a tuple."
         assert type(self.max) is tuple, "max must be a tuple."
+
+@dataclasses.dataclass
+class ImageSampler_T(Sampler_T):
+    func: function = lambda x: x
+
+@dataclasses.dataclass
+class NormalMapSampler_T(Sampler_T):
+    func: function = lambda x:x
