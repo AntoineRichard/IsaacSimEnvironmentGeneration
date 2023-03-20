8862e0312f649b1feb692c68443118f493f15698
jnskkmhr <junjun.k2012@gmail.com>
2023-03-20 14:21:55 +0900

    add image clipper and normalmap clipper

---
+++ WorldBuilders/Clippers.py
@@ -0,0 +1,96 @@
+import numpy as np
+import quaternion
+from Types import *
+
+# I was in troublem deciding whether I should wrap BaseSampler or create new class and wrap it. 
+# Since Z-value clip function is deterministic, meaning z value is always determined based on heightmap. 
+# Therefore, there is no point in creating random distribution as done in BaseSampler and other wrapped class. 
+# So, I made BaseClipper method and new class called HeightClipper wraps BaseClipper. 
+
+class BaseClipper:
+    def __init__(self, sampler_cfg: Sampler_T):
+        self._sampler_cfg = sampler_cfg
+        self.image = self._sampler_cfg.data
+        self.resolution = self._sampler_cfg.resolution
+        self.mpp_resolution = self._sampler_cfg.mpp_resolution
+
+        assert len(self.image.shape) == 2, f"image need to be 1 channel image, not {image.shape}"
+    
+    def __call__(self, **kwargs):
+        return self.sample(**kwargs)
+
+    def sample(self, **kwargs):
+        raise NotImplementedError()
+
+class HeightClipper(BaseClipper):
+    def __init__(self, sampler_cfg: Sampler_T):
+        super().__init__(sampler_cfg)
+
+    def sample(self, query_point:np.ndarray, **kwargs):
+        """
+        query point is (x, y) point generated from sampler
+        """
+        x = query_point[:, 0]
+        y = query_point[:, 1]
+        H, W = self.resolution
+        us = x // self.mpp_resolution + (W // 2) * np.ones_like(x)
+        vs = (H // 2) * np.ones_like(y) - y // self.mpp_resolution
+        images = []
+        for u, v in zip(us, vs):
+            u = int(u)
+            v = int(v)
+            images.append(self.image[v, u])
+        return np.stack(images)[:, np.newaxis]
+
+class NormalMapClipper(BaseClipper):
+    def __init__(self, sampler_cfg: Sampler_T):
+        super().__init__(sampler_cfg)
+
+    def compute_slopes(self)->None:
+        nx,ny = np.gradient(self.image)
+        slope_x = np.arctan2(nx,1) #theta_x = tan^-1(nx)
+        slope_y = np.arctan2(ny,1) #theta_y = tan^-1(ny)
+        # magnitude = np.hypot(nx,ny)
+        # slope_xy = np.arctan2(magnitude,1)
+        self.slope_x = slope_x
+        self.slope_y = slope_y
+        # self.slope_xy = slope_xy
+        # self.magnitude = magnitude
+
+    def sample(self, query_point:np.ndarray, **kwargs):
+        """
+        query point is (x, y) point generated from sampler
+        """
+        self.compute_slopes()
+        x = query_point[:, 0]
+        y = query_point[:, 1]
+        H, W = self.resolution
+        us = x // self.mpp_resolution + (W // 2) * np.ones_like(x)
+        vs = (H // 2) * np.ones_like(y) - y // self.mpp_resolution
+        quat = []
+        for u, v in zip(us, vs):
+            u = int(u)
+            v = int(v)
+            roll = self.slope_x[v, u]
+            pitch = self.slope_y[v, u]
+            yaw = 0.0
+            q = quaternion.as_float_array(quaternion.from_euler_angles([roll, pitch, yaw]))
+            quat.append(q)
+
+        return np.stack(quat)
+
+class ClipperFactory:
+    def __init__(self):
+        self.creators = {}
+    
+    def register(self, name: str, class_: BaseClipper) -> None:
+        self.creators[name] = class_
+        
+    def get(self, cfg: Sampler_T, **kwargs:dict) -> BaseClipper:
+        if cfg.__class__.__name__ not in self.creators.keys():
+            raise ValueError("Unknown sampler requested.")
+        return self.creators[cfg.__class__.__name__](cfg)
+
+Clipper_Factory = ClipperFactory()
+Clipper_Factory.register("ImageClipper_T", HeightClipper)
+Clipper_Factory.register("NormalMapClipper_T", NormalMapClipper)
---
+++ WorldBuilders/test_image_clip.py
@@ -0,0 +1,77 @@
+from omni.isaac.kit import SimulationApp
+import numpy as np
+import os
+
+simulation_app = SimulationApp({"headless": False})
+
+from omni.isaac.core import World
+from pxr_utils import createInstancerAndCache, setInstancerParameters, setRotateXYZ, createObject, addCollision
+from Mixer import *
+import omni
+from pxr import UsdGeom, Sdf, UsdLux, Gf, UsdShade
+
+root_dir = "/home/lunar4/jnskkmhr/omn_asset" # working directory
+asset_path = "rock_model" # asset root dir
+
+world = World(stage_units_in_meters=1.0)
+stage = omni.usd.get_context().get_stage()
+
+distant_light = UsdLux.DistantLight.Define(stage, "/sun")
+distant_light.GetIntensityAttr().Set(10000)
+xform = UsdGeom.Xformable(distant_light)
+setRotateXYZ(xform, Gf.Vec3d(0,85,0))
+
+assets = os.listdir(os.path.join(root_dir, asset_path))
+assets = [os.path.join(os.path.join(root_dir, asset_path), asset) for asset in assets if asset.split('.')[-1]=="usd"]
+createInstancerAndCache(stage, "/rocks", assets)
+
+# load base terrain prim
+terrain_prim_path = os.path.join(root_dir, "Terrain/LRO_NAC_DEM_73N350E_150cmp_3500_4000_2000_2500_USD_clean.usd")
+createObject("/terrain", stage, terrain_prim_path)
+addCollision(stage, "/terrain")
+
+# load sample dem file
+# 1.5 m/pix
+img_path = "/home/lunar4/jnskkmhr/IsaacSimEnvironmentGeneration/DEM/LRO_NAC_DEM_73N350E_150cmp_3500_4000_2000_2500.npy"
+data = np.load(img_path)
+# data = np.load("/home/lunar4/jnskkmhr/DEM/generated_tile.npy")
+H, W = data.shape
+mpp = 1.5
+offset = 20
+# need some offset (little bit like 10m) for out of boundary error
+xmin = -((W//2)*mpp - offset)
+xmax = (W//2)*mpp - offset
+ymin = -((H//2)*mpp - offset)
+ymax = (H//2)*mpp -offset
+
+# sampler = NormalSampler_T(mean=(0.0, 0.0), std=(xmax/2, ymax/2), randomization_space=2, seed=77)
+sampler = UniformSampler_T(min=(xmin, ymin), max=(xmax, ymax), randomization_space=2, seed=77)
+plane = Plane_T(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, output_space=2)
+
+image_layer = Image_T(output_space=1)
+image_clipper = ImageClipper_T(randomization_space=1, resolution=(H, W), mpp_resolution=mpp, data=data) #only z
+
+normalmap_layer = NormalMap_T(output_space=4)
+normalmap_clipper = NormalMapClipper_T(randomization_space=4, resolution=(H, W), mpp_resolution=mpp, data=data)
+
+uni1 = UniformSampler_T(randomization_space=1)
+line = Line_T(xmin=30.0, xmax=32.0)
+
+req_pos1 = UserRequest_T(p_type = Position_T(), sampler=sampler, layer=plane, axes=["x","y"])
+req_pos2 = UserRequest_T(p_type = Position_T(), sampler=image_clipper, layer=image_layer, axes=["z"])
+req_ori = UserRequest_T(p_type = Orientation_T(), sampler=normalmap_clipper, layer=normalmap_layer, axes=["x", "y", "z", "w"])
+req_scale = UserRequest_T(p_type = Scale_T(), sampler=uni1, layer=line, axes=["xyz"])
+requests = [req_pos1, req_pos2, req_ori, req_scale]
+mixer = RequestMixer(requests)
+
+num = 10000
+attributes = mixer.executeGraph(num)
+position = attributes["xformOp:translation"]
+scale = attributes["xformOp:scale"]
+orientation = attributes["xformOp:orientation"]
+setInstancerParameters(stage, "/rocks", pos=position, quat=orientation, scale=scale)
+
+while(True):
+    world.step(render=True)
+
+simulation_app.close()
